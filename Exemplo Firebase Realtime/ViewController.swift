73305c6afab25dce554b3181a8f6d6820c7ebd8b
Mike Andrade <mikewebbr@gmail.com>
2018-08-28 21:29:07 -0300

    Initial Commit

---
+++ Exemplo Firebase Realtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exemplo Firebase Realtime
+//
+//  Created by Ricardo on 28/08/2018.
+//  Copyright © 2018 Hands Midia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
